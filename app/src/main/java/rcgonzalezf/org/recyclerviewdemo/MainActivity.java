f339dc71dc34fac0c0180ef74df8748b0c087d37
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-07-27 21:47:43 -0700

    Adding empty RecyclerView

--- app/src/main/java/rcgonzalezf/org/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/rcgonzalezf/org/recyclerviewdemo/MainActivity.java
@@ -2,15 +2,25 @@ package rcgonzalezf.org.recyclerviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
+
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
     }
 
     @Override
